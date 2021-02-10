c6d289e426228caaea9ad70a2ff5c9ac35366ac3
jacktgentile <32143117+jacktgentile@users.noreply.github.com>
2021-02-10 00:21:03 -0600

    initial release

---
+++ src/Assets/Scripts/GUI Scripts/NameToTitle.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NameToTitle : MonoBehaviour {
+
+	public Text title;
+
+
+	void OnMouseEnter()
+	{
+		switch(name)
+		{
+		case "Pac-Man":
+			title.color = Color.yellow;
+			title.text = "Robinhood";
+			break;
+
+		case "Blinky":
+			title.color = Color.red;
+			title.text = "Marky";
+			break;
+
+		case "Pinky":
+			title.color = new Color(254f/255f, 152f/255f, 203f/255f);
+			title.text = "Jeffy";
+			break;
+
+		case "Inky":
+			title.color = Color.cyan;
+			title.text = "Musky";
+			break;
+
+		case "Clyde":
+			title.color = new Color(254f/255f, 203f/255f, 51f/255f);
+			title.text = "Cheeto";
+			break;
+		}
+	}
+
+	void OnMouseExit()
+	{
+		title.text = "Pac-Man Clone";
+		title.color = Color.white;
+	}
+}
